5e109282637a7d8b436779049c381b7b03efe835
clefort <camille.lefort@emundus.fr>
2025-07-29 09:17:57 +0200

    debug railway img

--- services/auth-service/src/server.ts
+++ services/auth-service/src/server.ts
@@ -2,17 +2,30 @@ import app from "./app";
 import sequelize from "./config/database";
 import logger from "./utils/logger";
 
+import fs from "fs";
+import path from "path";
+
+const imgPath = path.resolve(__dirname, "../public/img");
+
+fs.readdir(imgPath, (err, files) => {
+  if (err) {
+    console.error("Erreur lecture img :", err);
+  } else {
+    console.log("Fichiers dans /public/img :", files);
+  }
+});
+
 const PORT = Number(process.env.PORT) || 3001;
 
 (async () => {
-    try {
-        await sequelize.authenticate();
-        logger.info("Connexion DB réussie");
+  try {
+    await sequelize.authenticate();
+    logger.info("Connexion DB réussie");
 
-        app.listen(PORT, () => {
-            logger.info(`Serveur lancé sur http://localhost:${PORT}`);
-        });
-    } catch (err) {
-        logger.error("Impossible de se connecter à la DB :", err);
-    }
+    app.listen(PORT, () => {
+      logger.info(`Serveur lancé sur http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    logger.error("Impossible de se connecter à la DB :", err);
+  }
 })();
