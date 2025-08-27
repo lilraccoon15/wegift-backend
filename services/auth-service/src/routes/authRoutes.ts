36e6adc95098cac29d06c53af96b302a6eb1b08c
clefort <camille.lefort@emundus.fr>
2025-08-27 12:09:29 +0200

    fix google callback

--- services/auth-service/src/routes/authRoutes.ts
+++ services/auth-service/src/routes/authRoutes.ts
@@ -98,6 +98,7 @@ router.get(
   "/oauth/google/callback",
   passport.authenticate("google", {
     session: false,
+    failureRedirect: `${process.env.FRONTEND_URL}/login?error=google`,
   }),
   handleGoogleCallback
 );
